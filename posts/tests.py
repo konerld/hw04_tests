d7ac84e23834e73da2119881baae45c2a732ee6b
Волков Дмитрий Алексеевич <volkovda@pochtabank.ru>
2020-07-21 23:46:16 +0300

    первая попытка сдать проект

--- posts/tests.py
+++ posts/tests.py
@@ -91,4 +91,18 @@ class PageTest(TestCase):
                          response.context['post'],
                          "Пост не отображается на собственной странице!")
 
-    
+    def test_auth_user_can_edit_own_post(self):
+        """
+        Тест проверяет, что авторизованный пользователь может отредактировать
+        свой пост и его содержимое изменится на всех связанных страницах
+        """
+        self.client.login(username='skywalker', password='123456')
+        response = self.client.get(f'/skywalker/{self.post.id}/edit/')
+        self.assertEqual(response.status_code,
+                         200,
+                         'У пользователя нет доступа к '
+                         'редактированию своего поста!')
+        new_text = 'This is text after edit.'
+        response = self.client.post(f'/skywalker/{self.post.id}/edit/', {'text': new_text})
+        response = self.client.get(f'/skywalker/{self.post.id}/edit/')
+        self.assertIn(bytes(new_text, encoding='UTF-8'), response.content)
--- posts/views.py
+++ posts/views.py
@@ -64,7 +64,6 @@ def profile(request, username):
 
 def post_view(request, username, post_id):
     author = get_object_or_404(User, username=username)
-    # post = Post.objects.filter(pk=post_id)
     post = get_object_or_404(Post, pk=post_id)
     return render(
         request,
@@ -78,41 +77,18 @@ def post_view(request, username, post_id):
 
 @login_required
 def post_edit(request, username, post_id):
-    author = get_object_or_404(User, username=username)
-    post_obj = get_object_or_404(Post, pk=post_id, author=author)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post_obj)
-        if form.is_valid():
-            form.save()
-            return redirect('post', username=username, post_id=post_id)
-        return render(
-            request,
-            'post_new.html',
-            {
-                'form': form,
-                'post': post_obj
-            }
-        )
-    form = PostForm(instance=post_obj)
+    post = get_object_or_404(Post, id=post_id, author__username=username)
+    if request.user.username != username:
+        return redirect('post', username=username, post_id=post_id)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('post', username=username, post_id=post_id)
     return render(
         request,
         'post_new.html',
         {
             'form': form,
-            'post': post_obj
+            'post': post
         }
     )
-
-
-"""
-if request.method == 'POST':
-    form = PostForm(request.POST)
-    if form.is_valid():
-        post = form.save(commit=False)
-        post.author = request.user
-        post.save()
-        return redirect('index')
-    return render(request, 'new_post.html', {'form': form})
-form = PostForm()
-return render(request, 'new_post.html', {'form': form})
-"""

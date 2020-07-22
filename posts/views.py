33aa36c6817ce6ea7236ffa1a5270a15578e3d6b
Волков Дмитрий Алексеевич <volkovda@pochtabank.ru>
2020-07-23 00:54:50 +0300

    добавил includes

--- posts/views.py
+++ posts/views.py
@@ -67,7 +67,7 @@ def post_view(request, username, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(
         request,
-        'post.html',
+        'includes/post.html',
         {
             'author': author,
             'post': post
